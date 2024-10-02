57ae3389e5cd87698a4ec638bfcc6fa057f6d1c2
Carlos Y. Ruiz T. <cyair99@gmail.com>
2024-10-02 16:34:38 -0500

    add users clear

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
-from routers import users
+from routers import reports, users
 
 app = FastAPI()
 
 # Routers
 app.include_router(users.router)
+app.include_router(reports.router)
 
 @app.get("/")
 async def root():
--- routers/users.py
+++ routers/users.py
@@ -4,12 +4,20 @@ from jose import jwt, JWTError
 from passlib.context import CryptContext
 from datetime import datetime, timedelta, timezone
 
-from db.models.user import User, UserDB
-from db.client import db_client
+from db.users_db import users_db
+from models.user import User, UserDB
 
-ALGORITHM = "HS256"
-ACCESS_TOKEN_DURATION = 1
-SECRET_KEY = "9103666a0a35d46a90d693216b4bc8c4325de896aca45c6b5c02dfde5b67d3bd"
+from dotenv import load_dotenv
+import os
+
+from utils.auth import current_user
+from utils.search import serch_user_db
+
+load_dotenv()
+
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_DURATION = int(os.getenv("ACCESS_TOKEN_DURATION"))
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 router = APIRouter(
     prefix="/users", tags=["users"], responses={404: {"message": "Not found"}}
@@ -20,73 +28,21 @@ oauth2 = OAuth2PasswordBearer(tokenUrl="login")
 crypt = CryptContext(schemes=["bcrypt"])
 
 
-users_db = {
-    "raz": {
-        "username": "raz",
-        "email": "raz@mail.com",
-        "full_name": "Carlos Yair",
-        "disabled": False,
-        "password": "$2a$12$InI9NYsjLWSnjELL.cTDguV/HIv23tuESGmHIHjxuH6.LuEB3ugLy",
-    },
-    "jose": {
-        "username": "jose",
-        "email": "jose@mail.com",
-        "full_name": "Jose Perez",
-        "disabled": True,
-        "password": "$2a$12$5oOIm5763y4TeiYEP61MtOO5VyLo9kYJ6.rPMmlG5lVggadgJJCXS",
-    },
-}
+def generate_username(first_name: str, last_name: str) -> str:
+    username = ''.join([name[0].upper() for name in first_name.split() + last_name.split()])
 
-
-def serch_user_db(username: str):
-    user_data = users_db.get(username)
-    if isinstance(user_data, dict):
-        return UserDB(**user_data)
-    elif isinstance(user_data, UserDB):
-        return user_data
-    else:
-        raise ValueError("Invalid user data type")
-
-
-def serch_user(username: str):
-    user_data = users_db.get(username)
-    if isinstance(user_data, dict):
-        return User(**user_data)
-    elif isinstance(user_data, UserDB):
-        return user_data
-    else:
-        raise ValueError("Invalid user data type")
-
-
-async def auth_user(token: str = Depends(oauth2)):
-    exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Invalid credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-
-    try:
-        username = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM]).get("sub")
-        if username is None:
-            raise exception
-
-    except JWTError:
-        raise exception
-
-    return serch_user(username)
-
-
-async def current_user(user: User = Depends(auth_user)):
-    if user.disabled:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail="Inactive user"
-        )
-    return user
-
+    # Asegurarse de que el username sea único
+    initial_username = username
+    counter = 1
+    while username in users_db:
+        username = initial_username + str(counter)
+        counter += 1
+        
+    return username
 
 @router.post("/login")
 async def login(form: OAuth2PasswordRequestForm = Depends()):
-    user_db = users_db.get(form.username)
+    user_db = next((user for user in users_db if user.username == form.username), None)
     if not user_db:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST, detail="Incorrect username"
@@ -109,40 +65,37 @@ async def login(form: OAuth2PasswordRequestForm = Depends()):
 
     return {"access_token": acces_token, "token_type": "bearer"}
 
-
 @router.get("/me")
 async def me(user: User = Depends(current_user)):
     return user
-
 
 @router.get("/all")
 async def get_users(user: User = Depends(current_user)):
     return users_db
 
-
 @router.post("/", response_model=User, status_code=status.HTTP_201_CREATED)
-async def create_user(user: UserDB):
+async def create_user(user_db: UserDB, user: User = Depends(current_user)): 
 
-    db_client 
-
-    if user.username in users_db:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail="Username already exists"
-        )
+    # Generar username a partir de las iniciales
+    username = generate_username(user_db.first_name, user_db.last_name)
 
     # Hash the password
-    hashed_password = crypt.hash(user.password)
+    hashed_password = crypt.hash(user_db.password)
 
     # Create a new user object
     new_user = UserDB(
-        username=user.username,
-        email=user.email,
-        full_name=user.full_name,
-        disabled=user.disabled,
+        # Este cambiara a un valor dado por la base de datos
+        _id=str(len(users_db) + 1),
+        username= username,
+        full_name=user_db.first_name + " " + user_db.last_name,
+        first_name=user_db.first_name,
+        last_name=user_db.last_name,
+        disabled=user_db.disabled,
+        type=user_db.type,
         password=hashed_password,
     )
 
     # Add the new user to the users_db dictionary
-    users_db[user.username] = new_user
+    users_db.append(new_user)
 
     return new_user
